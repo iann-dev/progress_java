18431fd11c86d67731fcc4d2057088763dc682c8
Iann Carlos <159698398+iann-dev@users.noreply.github.com>
2024-02-13 08:40:18 -0300

    Update JokenpoArray.java

--- java_basic/java_array/JokenpoArray.java
+++ java_basic/java_array/JokenpoArray.java
@@ -6,30 +6,23 @@ public class JokenpoArray {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        System.out.println("Bem-vindo ao JOKENPÔ!");
-        System.out.println("Escolha: 1 - Pedra, 2 - Papel, 3 - Tesoura");
+        System.out.println("Bem-vindo ao JOKENPÔ!\nEscolha: 1 - Pedra, 2 - Papel, 3 - Tesoura");
 
         int escolhaUsuario = scanner.nextInt();
         if (escolhaUsuario < 1 || escolhaUsuario > 3) {
             System.out.println("Escolha inválida. Encerrando o jogo.");
-            scanner.close();
-            return;
-        }
-
-        String[] opcoes = {"Pedra", "Papel", "Tesoura"};
-        String escolhaUsuarioString = opcoes[escolhaUsuario - 1];
-        String escolhaComputadorString = opcoes[random.nextInt(3)];
-
-        System.out.println("Você escolheu: " + escolhaUsuarioString);
-        System.out.println("O computador escolheu: " + escolhaComputadorString);
-
-        if (escolhaUsuario == (random.nextInt(3) + 1)) {
-            System.out.println("Empate!");
-        } else if ((escolhaUsuario - escolhaComputadorString.length() == 1) || 
-                   (escolhaUsuario - escolhaComputadorString.length() == -2)) {
-            System.out.println("Você venceu!");
         } else {
-            System.out.println("Você perdeu!");
+            String[] opcoes = {"Pedra", "Papel", "Tesoura"};
+            String escolhaUsuarioString = opcoes[escolhaUsuario - 1];
+            String escolhaComputadorString = opcoes[random.nextInt(3)];
+
+            System.out.println("Você escolheu: " + escolhaUsuarioString +
+                               "\nO computador escolheu: " + escolhaComputadorString);
+
+            System.out.println(escolhaUsuario == (random.nextInt(3) + 1) ? "Empate!" :
+                               (escolhaUsuario - escolhaComputadorString.length() == 1 ||
+                                escolhaUsuario - escolhaComputadorString.length() == -2) ? "Você venceu!" :
+                               "Você perdeu!");
         }
 
         scanner.close();
