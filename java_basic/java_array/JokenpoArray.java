2faf244d813b38b76ddf13c888e0cd4f3a15d1d3
Iann Carlos <159698398+iann-dev@users.noreply.github.com>
2024-02-13 08:52:59 -0300

    Update JokenpoArray.java

--- java_basic/java_array/JokenpoArray.java
+++ java_basic/java_array/JokenpoArray.java
@@ -13,21 +13,17 @@ public class Jokenpo {
             System.out.println("Escolha inválida. Encerrando o jogo.");
         } else {
             String[] opcoes = {"Pedra", "Papel", "Tesoura"};
-            String escolhaUsuarioString = opcoes[escolhaUsuario - 1];
+            String escolhaUsuarioString = opcoes[--escolhaUsuario];
             String escolhaComputadorString = opcoes[random.nextInt(3)];
 
             System.out.printf("Você escolheu: %s\nO computador escolheu: %s\n",
                     escolhaUsuarioString, escolhaComputadorString);
 
-            int diferenca = (escolhaUsuario - 1) - (random.nextInt(3));
+            int diferenca = (escolhaUsuario - random.nextInt(3) + 2) % 3;
 
-            if (diferenca == 0) {
-                System.out.println("Empate!");
-            } else if (diferenca == 1 || diferenca == -2) {
-                System.out.println("Você venceu!");
-            } else {
-                System.out.println("Você perdeu!");
-            }
+            System.out.println(diferenca == 0 ? "Empate!" : 
+                               (diferenca == 1 || diferenca == 2) ? "Você venceu!" :
+                               "Você perdeu!");
         }
 
         scanner.close();
