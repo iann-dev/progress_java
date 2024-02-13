bdc240ff62e57614446c629441743263855a9c09
Iann Carlos <159698398+iann-dev@users.noreply.github.com>
2024-02-13 08:47:11 -0300

    Update JokenpoArray.java

--- java_basic/java_array/JokenpoArray.java
+++ java_basic/java_array/JokenpoArray.java
@@ -1,14 +1,14 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class JokenpoArray {
+public class Jokenpo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         System.out.println("Bem-vindo ao JOKENPÔ!\nEscolha: 1 - Pedra, 2 - Papel, 3 - Tesoura");
-
         int escolhaUsuario = scanner.nextInt();
+
         if (escolhaUsuario < 1 || escolhaUsuario > 3) {
             System.out.println("Escolha inválida. Encerrando o jogo.");
         } else {
@@ -16,13 +16,18 @@ public class JokenpoArray {
             String escolhaUsuarioString = opcoes[escolhaUsuario - 1];
             String escolhaComputadorString = opcoes[random.nextInt(3)];
 
-            System.out.println("Você escolheu: " + escolhaUsuarioString +
-                               "\nO computador escolheu: " + escolhaComputadorString);
+            System.out.printf("Você escolheu: %s\nO computador escolheu: %s\n",
+                    escolhaUsuarioString, escolhaComputadorString);
 
-            System.out.println(escolhaUsuario == (random.nextInt(3) + 1) ? "Empate!" :
-                               (escolhaUsuario - escolhaComputadorString.length() == 1 ||
-                                escolhaUsuario - escolhaComputadorString.length() == -2) ? "Você venceu!" :
-                               "Você perdeu!");
+            int resultado = (3 + escolhaUsuario - random.nextInt(3) - 1) % 3;
+
+            if (resultado == 0) {
+                System.out.println("Empate!");
+            } else if (resultado == 1) {
+                System.out.println("Você venceu!");
+            } else {
+                System.out.println("Você perdeu!");
+            }
         }
 
         scanner.close();
